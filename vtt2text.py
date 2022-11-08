2b42eb79b08742d5c4e257c4c5174055c2a97815
Rachmadani Haryono <foreturiga@gmail.com>
2022-11-08 12:58:36 +0800

    feat: clean_format2

--- vtt2text.py
+++ vtt2text.py
@@ -1,6 +1,9 @@
 # @Author: anh-tuan.vu
 # @Date:   2021-01-27 07:02:40
 # @Last Modified by: rachmadani haryono
+import dataclasses
+import enum
+import pathlib
 import re
 from os.path import exists, splitext
 
@@ -75,8 +78,105 @@ def to_file(file_in: str, file_out=None, **kwargs) -> str:
     return file_out
 
 
-def clean_format2(src):
-    pass
+class LineType(enum.Enum):
+    TEXT = 1
+    EMPTY_LINE = 2
+    TIME = 3
+    TEXT_WITH_TIME = 4
+
+
+@dataclasses.dataclass
+class LineData:
+    """LineData class.
+
+    Attributes:
+        time_start: first time match integer rounded in second
+
+    """
+
+    line: str
+    format_: LineType = LineType.TEXT
+    line_clean: str = None
+    time_start: int = None
+
+    def __post_init__(self):
+        if not self.line:
+            self.format_ = LineType.EMPTY_LINE
+        if match := re.match(r"^(\d*):(\d*):(\d*)(\.\d*)?", self.line):
+            self.format_ = LineType.TIME
+            self.time_start = (
+                (int(match.groups()[0]) * 3600)
+                + int(match.groups()[1]) * 60
+                + int(match.groups()[2])
+            )
+        patt1 = r"<(\d*:\d*:\d*(\.\d*)?|/?c)>"
+        if re.search(patt1, self.line):
+            self.format_ = LineType.TEXT_WITH_TIME
+            self.line_clean = re.sub(
+                r"\s{2,}", " ", re.sub(patt1, " ", self.line)
+            ).strip()
+            if match := re.match(r".*<(\d*):(\d*):(\d*)(\.\d*)?>", self.line):
+                self.time_start = (
+                    (int(match.groups()[0]) * 3600)
+                    + int(match.groups()[1]) * 60
+                    + int(match.groups()[2])
+                )
+        if self.format_ == LineType.TEXT:
+            self.line_clean = self.line.strip()
+
+
+def clean_format2(src: str):
+    """clean with format2 for logseq."""
+    data_list = []
+    first_time_found = False
+    for line in pathlib.Path(src).read_text().splitlines():
+        entry = LineData(line=line.strip())
+        if not first_time_found:
+            if entry.format_ not in (LineType.TEXT_WITH_TIME, LineType.TIME):
+                # skip text until first time match line
+                continue
+            else:
+                first_time_found = True
+                data_list.append((LineType.TIME, entry.time_start))
+        else:
+            if entry.format_ == LineType.TEXT:
+                data_list.append((LineType.TEXT, entry.line_clean))
+            elif entry.format_ == LineType.EMPTY_LINE:
+                pass
+            elif entry.format_ == LineType.TIME:
+                data_list.append((LineType.TIME, entry.time_start))
+            elif entry.format_ == LineType.TEXT_WITH_TIME:
+                data_list.append((LineType.TIME, entry.time_start))
+                data_list.append((LineType.TEXT, entry.line_clean))
+            else:
+                raise ValueError(f"Unknown line format: {entry.format_}")
+    last_time = None
+    last_item = None
+    line_list = []
+    for idx, item in enumerate(data_list):
+        if idx != 0:
+            last_item = data_list[idx - 1]
+        if item[0] == LineType.TIME:
+            #  skip if next item is also time
+            #  or last time is the same as current time
+            if (data_list[idx + 1][0] == LineType.TIME) or (
+                last_time is not None and last_time == item[1]
+            ):
+                continue
+            else:
+                line_list.append("")
+                line_list.append("".join(("{{youtube-timestamp ", str(item[1]), "}}")))
+                last_time = item[1]
+        elif (
+            idx == 0
+            or last_item != LineType.TEXT
+            or (last_item[0] == LineType.TEXT and last_item[1] != item[1])
+        ) and item[1] != line_list[-1].strip():
+            #  only print if it is first time on element
+            # last item is not text
+            #  or item is not the same as the last one
+            line_list.append(item[1].strip())
+    return "\n".join(line_list)
 
 
 @click.group()
@@ -96,14 +196,18 @@ def cli(debug):
 )
 @click.option("--output", type=click.File("wb"), help="Output filename")
 def run_clean(src, format_, output):
-    if format_ == "1" and not output:
-        print(clean(src))
+    content = None
+    if format_ == "1":
+        if not output:
+            content = clean(src)
+        else:
+            raise NotImplementedError
     elif format_ == "2":
-        content = clean_format2(src)
+        content = clean_format2(src).strip()
         if output:
             # write to output
-            pass
-        else:
-            print(content)
+            raise NotImplementedError
     else:
-        raise ValueError(f"Unknown error: {format_}")
+        raise ValueError(f"Unknown format: {format_}")
+    if not output:
+        print(content)
